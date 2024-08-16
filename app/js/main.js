2ea0d49f68bc10f406e6c7ae723b175a197f1773
Kataru <belogorod5@gmail.com>
2024-08-16 20:20:07 +0300

    new section 'slider' 'mobile'

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
-    $('.').slick(
+    $('.slider__items').slick(
         {
+            dots: true,
             infinite: true,
+            speed: 600,
             slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToScroll: 3,
+            responsive: [
+                {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+                },
+                {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
+                }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                }
+            ]
         }
     );
 });
